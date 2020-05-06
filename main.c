a4c41820d0793afe04fd0a4801d57b83b2ffd22e
later <1638414737@qq.com>
2020-05-06 20:21:33 +0800

    finished

--- main.c
+++ main.c
@@ -21,7 +21,7 @@
 #define uint unsigned int
 #endif
 
-uchar bluetooth[11];
+uchar bluetooth[13];
 uchar code CDIS1[14]={"BlueTooth Data"};
 
 void main()
@@ -36,14 +36,12 @@ void main()
 		/*读取DHT11温湿度数据*/
 		DHT11_delay_ms(1500);    //DHT11上电后要等待1S以越过不稳定状态在此期间不能发送任何指令
 		DHT11_receive();  
-//		send_string(rec_dat);    //RH:湿度, CU:温度
-//		send_string("\r\n");
+
 
 		/*读取ADC0809读取光强数值*/
 		delayms(10);		//80MS
 		Read_Light();	
-//		send_string(light);    	
-//		send_string("\r\n");
+
 		
 		/*LCD1602显示传感器数组*/
 		LcdWriteCom(0x01);  //清屏
@@ -57,26 +55,36 @@ void main()
 		{
 			LcdWriteData(rec_dat[i]);		
 		}
+		for(i=0;i<2;i++)
+		{
+			LcdWriteData(' ');		
+		}
 		for(i=0;i<3;i++)
 		{
 			LcdWriteData(light[i]);	
 		}
+		LcdWriteData('L');	
+		LcdWriteData('u');	
+		
 		
 		bluetooth[0]='#';
-		bluetooth[1]=rec_dat[0];
-		bluetooth[2]=rec_dat[1];
-		bluetooth[3]=' ';
-		bluetooth[4]=rec_dat[6];
-		bluetooth[5]=rec_dat[7];
-		bluetooth[6]=' ';
-		bluetooth[7]=light[0];
-		bluetooth[8]=light[1];
-		bluetooth[9]=light[2];
-		bluetooth[10]='#';
+		bluetooth[1]=' ';
+		bluetooth[2]=rec_dat[0];
+		bluetooth[3]=rec_dat[1];
+		bluetooth[4]=' ';
+		bluetooth[5]=rec_dat[6];
+		bluetooth[6]=rec_dat[7];
+		bluetooth[7]=' ';
+		bluetooth[8]=light[0];
+		bluetooth[9]=light[1];
+		bluetooth[10]=light[2];
+		bluetooth[11]=' ';
+		bluetooth[12]='*';
 		
 		/*串口蓝牙发送数据*/
 		send_string(bluetooth);
 		send_string("\r\n");
+		delayms(20);		//80MS
 	
 	}
 }
